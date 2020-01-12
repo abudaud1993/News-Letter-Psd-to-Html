88cf27579304af19ba132fb0788e13fe2bb561d3
unknown <you@example.com>
2020-01-13 01:07:06 +0600

    News Letter PSD TO HTML completed

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+
+  $('#slider').nivoSlider({
+    // effect: 'random',
+    // slices: 15,
+    // boxCols: 8,
+    // boxRows: 4,
+    // animSpeed: 500,
+    // pauseTime: 3000,
+    // startSlide: 0,
+    // directionNav: true,
+    controlNav: false, // true
+    // controlNavThumbs: false,
+    // pauseOnHover: true,
+    // manualAdvance: false,
+    prevText: ' ',
+    nextText: ' ',
+    // randomStart: false,
+    });
+
+
+  
+
+
+
+})(jQuery);
